600bd737921cd63be473768998ca7cd0078bf61e
Kaito-Momoi <k-momoi@evangsol.co.jp>
2025-08-20 15:17:13 +0900

    chore: handle missing root element

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const container = document.getElementById('root');
+
+if (!container) {
+  throw new Error('Root container missing');
+}
+
+const root = ReactDOM.createRoot(container);
+
 root.render(
   <React.StrictMode>
     <App />
